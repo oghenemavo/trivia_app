f355d3e71b04e91007bb641440d6059e0d6ab686
Mavi Onogomuho <mavi.onogomuho@titantrustbank.com>
2022-09-20 17:54:57 +0100

    delete question route setup

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -93,6 +93,28 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+    @app.route("/questions/<int:question_id>", methods=["DELETE"])
+    def delete_question(question_id):
+        try:
+            question = Question.query.filter(Question.id == question_id).one_or_none()
+
+            if question is None:
+                abort(404)
+
+            question.delete()
+            selection = Question.query.order_by(Question.id).all()
+            current_question = paginate_questions(request, selection)
+
+            return jsonify(
+                {
+                    "success": True,
+                    "deleted": question_id,
+                    "books": current_question,
+                }
+            )
+
+        except:
+            abort(422)
 
     """
     @TODO:
@@ -122,9 +144,6 @@ def create_app(test_config=None):
                 answer=new_answer,
             )
             question.insert()
-
-            # selection = Question.query.order_by(Book.id).all()
-            # current_books = paginate_books(request, selection)
 
             return jsonify(
                 {
